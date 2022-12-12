9d07d3af084d6385cdd9b7286550cd4cebd8a396
paosua86 <paolasuarezb1986@gmail.com>
2022-12-12 11:43:28 -0500

    0 working

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Write a Python script that, using this REST API, for a given employee ID, returns
+information about his/her todo list progress"""
+
+
+if __name__ == "__main__":
+
+    from sys import argv
+    import requests
+
+    todos = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(argv[1]))
+    data_user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(argv[1]))
+
+    task_done = 0
+    all_tasks = 0
+    task_done_list = []
+
+    for task in todos.json():
+        all_tasks += 1
+        if task['completed'] is True:
+            task_done += 1
+            task_done_list.append(task['title'])
+
+    employee_name = data_user.json()['name']
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, task_done, all_tasks))
+
+    for task in task_done_list:
+        print("\t {}".format(task))
+
